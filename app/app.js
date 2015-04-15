763090c64b630788a326cc359d1449f7e6588e22
Andros Rosa <andros2011@gmail.com>
2015-04-14 22:17:29 -0400

    gemStore: cherry-pick angular course source
    * basically replaced the app directory, and tweaks to project
    settings.

--- app/app.js
+++ app/app.js
@@ -1,12 +1,58 @@
-'use strict';
+(function() {
+  'use strict';
+  var app = angular.module('gemStore', ['store-directives']);
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  app.controller('GalleryController', function() {
+    this.imageIndex = 0;
+    this.setCurrent = function(imageNumber) {
+      console.log(imageNumber);
+      this.imageIndex = imageNumber || 0;
+    };
+  });
+
+  app.controller('StoreController', function() {
+    this.products = gems;
+  });
+
+  var gems = [{
+    name: 'Azurite',
+    description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+    shine: 8,
+    price: 110.50,
+    rarity: 7,
+    color: '#CCC',
+    faces: 14,
+    images: [
+      "http://cl.zdn.io/UuY7/gem-02.gif",
+      "http://cl.zdn.io/UuWP/gem-05.gif",
+      "http://cl.zdn.io/UuD4/gem-09.gif"
+    ]
+  }, {
+    name: 'Bloodstone',
+    description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+    shine: 9,
+    price: 22.90,
+    rarity: 6,
+    color: '#EEE',
+    faces: 12,
+    images: [
+      "http://cl.zdn.io/UvAK/gem-01.gif",
+      "http://cl.zdn.io/Uuwd/gem-03.gif",
+      "http://cl.zdn.io/UuVP/gem-04.gif",
+    ]
+  }, {
+    name: 'Zircon',
+    description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+    shine: 70,
+    price: 1100,
+    rarity: 2,
+    color: '#000',
+    faces: 6,
+    images: [
+      "http://cl.zdn.io/Uv3v/gem-06.gif",
+      "http://cl.zdn.io/Uu84/gem-07.gif",
+      "http://cl.zdn.io/Uuky/gem-10.gif"
+    ]
+  }];
+
+})();
---
+++ app/products.js
@@ -0,0 +1,35 @@
+var app = angular.module('store-directives', []);
+
+app.directive("descriptions", function() {
+  return {
+    restrict: "E",
+    templateUrl: "descriptions.html"
+  }
+});
+
+app.directive("specs", function() {
+  return {
+    restrict: "E",
+    templateUrl: "specs.html"
+  }
+});
+
+app.directive("productTabs", function() {
+  return {
+    restrict: "E",
+
+    templateUrl: "product-tabs.html",
+    controller: function() {
+      this.tab = 1;
+
+      this.isSet = function(checkTab) {
+        return this.tab === checkTab;
+      };
+
+      this.setTab = function(activeTab) {
+        this.tab = activeTab;
+      };
+    },
+    controllerAs: "tab"
+  };
+});
